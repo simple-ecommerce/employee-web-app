1280c449b2138d8dad019ac0fb70679a3b323622
ArturLuisOliveira <arturluisoliveira@gmail.com>
2022-12-08 20:28:03 -0300

    add table template into item details page

--- src/pages/catalog/item/view/components/info_list/InfoList.tsx
+++ src/pages/catalog/item/view/components/info_list/InfoList.tsx
@@ -1,46 +1,50 @@
 import { Table, Typography } from "antd";
+import { Template } from "../../../../../../components";
 import { ItemLabels } from "../../../../../../constants/labels/ItemLabels";
 import { ItemModel } from "../../../../../../services/api/catalog/models/ItemModel";
 
 export const InfoList = ({ item }: { item?: ItemModel }) => {
   return (
-    <>
-      {" "}
-      <Typography.Title level={5}>Info</Typography.Title>
-      <Table
-        columns={[
-          {
-            title: "Name",
-            dataIndex: "name",
-            key: "name",
-          },
-          {
-            title: "Value",
-            dataIndex: "value",
-            key: "value",
-          },
-        ]}
-        pagination={false}
-        showHeader={false}
-        dataSource={[
-          "id",
-          "name",
-          "shortDescription",
-          "longDescription",
-          "price",
-          "sku",
-          "ean",
-          "upc",
-          "gtin",
-          "brand",
-          "createdAt",
-          "updatedAt",
-        ].map((key) => ({
-          key,
-          name: ItemLabels[key as keyof ItemModel] ?? key,
-          value: item?.[key as keyof typeof item] ?? "-",
-        }))}
+    <Template.Table>
+      <Template.Table.Header
+        left={<Typography.Title level={5}>Info</Typography.Title>}
       />
-    </>
+      <Template.Table.Content>
+        <Table
+          columns={[
+            {
+              title: "Name",
+              dataIndex: "name",
+              key: "name",
+            },
+            {
+              title: "Value",
+              dataIndex: "value",
+              key: "value",
+            },
+          ]}
+          pagination={false}
+          showHeader={false}
+          dataSource={[
+            "id",
+            "name",
+            "shortDescription",
+            "longDescription",
+            "price",
+            "sku",
+            "ean",
+            "upc",
+            "gtin",
+            "brand",
+            "createdAt",
+            "updatedAt",
+          ].map((key) => ({
+            key,
+            name: ItemLabels[key as keyof ItemModel] ?? key,
+            value: item?.[key as keyof typeof item] ?? "-",
+          }))}
+        />
+      </Template.Table.Content>
+    </Template.Table>
   );
 };
--- src/pages/catalog/item/view/components/specifications_list/SpecificationsList.tsx
+++ src/pages/catalog/item/view/components/specifications_list/SpecificationsList.tsx
@@ -1,32 +1,37 @@
 import { Table, Typography } from "antd";
+import { Template } from "../../../../../../components";
 
 export const SpecificationsList = () => {
   return (
-    <>
-      <Typography.Title level={5}>Specifications</Typography.Title>
-      <Table
-        columns={[{ title: "Name", dataIndex: "name", key: "name" }]}
-        // rowSelection={{ ...rowSelection, checkStrictly }}
-        dataSource={[
-          {
-            key: 1,
-            name: "Color",
-            children: [
-              { key: 2, name: "Red" },
-              { key: 3, name: "Blue" },
-            ],
-          },
-          {
-            key: 4,
-            name: "Size",
-            children: [
-              { key: 5, name: "S" },
-              { key: 6, name: "M" },
-              { key: 7, name: "L" },
-            ],
-          },
-        ]}
+    <Template.Table>
+      <Template.Table.Header
+        left={<Typography.Title level={5}>Specifications</Typography.Title>}
       />
-    </>
+      <Template.Table.Content>
+        <Table
+          columns={[{ title: "Name", dataIndex: "name", key: "name" }]}
+          // rowSelection={{ ...rowSelection, checkStrictly }}
+          dataSource={[
+            {
+              key: 1,
+              name: "Color",
+              children: [
+                { key: 2, name: "Red" },
+                { key: 3, name: "Blue" },
+              ],
+            },
+            {
+              key: 4,
+              name: "Size",
+              children: [
+                { key: 5, name: "S" },
+                { key: 6, name: "M" },
+                { key: 7, name: "L" },
+              ],
+            },
+          ]}
+        />
+      </Template.Table.Content>
+    </Template.Table>
   );
 };
